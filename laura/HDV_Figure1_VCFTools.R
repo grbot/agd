74733618108b37230f3ed0d0de05cd505d2722f7
Laura R. Botigué <rb.laura@gmail.com>
2017-06-16 15:41:38 +0100

    simplified code, fixed HDV bug

--- laura/HDV_Figure1_VCFTools.R
+++ laura/HDV_Figure1_VCFTools.R
@@ -1,19 +1,19 @@
 
-findCommonVars <- function(varCol1, varCol2) {
-    match <- intersect(varCol1, varCol2)
-    rowPop1 <- which(varCol1 %in% match)
-    rowPop2 <- which(varCol2 %in% match)
+findCommonVars <- function(varPop1, varPop2) {
+    match <- intersect(varPop1, varPop2)
+    rowPop1 <- which(varPop1 %in% match)
+    rowPop2 <- which(varPop2 %in% match)
     print(length(match))
     print(length(rowPop1))
     print(length(rowPop2))
     return(list(rowPop1  = rowPop1, rowPop2 = rowPop2))
 }
 
-includeBiallelicOnly <- function(varCol) {
-    dupVariant <- varCol[which(duplicated(varCol) == TRUE)]
+includeBiallelicOnly <- function(varPop) {
+    dupVariant <- varPop[which(duplicated(varPop) == TRUE)]
     if (length(dupVariant) > 0) {
-        removeDup <- which(varCol %in% dupVariant)
-        return(removeDup)
+        biallelic <- varPop[-which(varPop %in% dupVariant)]
+        return(biallelic)
     }
 }
 
@@ -22,9 +22,16 @@ yMaxLim <- function(x, base)
   base * round(x/base) 
 }
 
+extractFrequencies <- function(frqFileColumn)
+{
+    colSplit <- strsplit(frqFileColumn, split=":")
+    frq <- as.numeric(sapply(colSplit, "[[", 2))
+    return(frq)
+}
+
 
 frqFileExtension <- "_dp6_anc_f_dbsnp_snpeff.daf.frq"
-delta = 0.6
+delta = 0.4
 popList <- c('Mali', 'Zambia') # Here you need to include the names of all your populations.
 HDVmat <- matrix(0, nrow=length(popList), ncol=length(popList),dimnames = list(popList,popList))
 for (pop1 in 1:(length(popList)-1)) {
@@ -35,41 +42,31 @@ for (pop1 in 1:(length(popList)-1)) {
     HDVcounter=0 ## This will tells us whether we have HDVs at all, to write an output log file
     out <- c()
     for (chr in 22:22) {
-      pop1FrqFile <- read.delim2(paste(popList[pop1], "_phased_chr", chr, frqFileExtension, sep=""), skip=1, header=T)
-      pop2FrqFile <- read.delim2(paste(popList[pop2], "_phased_chr", chr, frqFileExtension, sep=""), skip=1, header=T)
-      varCol1 <- as.vector(pop1FrqFile[,2])
-      varCol2 <- as.vector(pop2FrqFile[,2])
-      duplicatedRow1 <- includeBiallelicOnly(varCol1)
-      duplicatedRow2 <- includeBiallelicOnly(varCol2)
-      if (length(duplicatedRow1) > 0) {
-          pop1FrqFile <- pop1FrqFile[-duplicatedRow1,]
-          varCol1 <- varCol1[-duplicatedRow1]
-      }
-      if (length(duplicatedRow2) > 0) {
-         pop2FrqFile <- pop2FrqFile[-duplicatedRow2,]
-         varCol2 <- varCol2[-duplicatedRow2]
-      }
-      if (setequal(varCol1, varCol2) == FALSE) {
-          selectRows = findCommonVars(varCol1, varCol2)
-          pop1FrqDf = pop1FrqFile[selectRows[[1]],]
-          pop2FrqDf = pop2FrqFile[selectRows[[2]],]
+      pop1FrqFile <- read.delim2(paste(popList[pop1], "_phased_chr", chr, frqFileExtension, sep=""), skip=1, header=T, stringsAsFactors=FALSE)
+      pop2FrqFile <- read.delim2(paste(popList[pop2], "_phased_chr", chr, frqFileExtension, sep=""), skip=1, header=T, stringsAsFactors=FALSE)
+      varPop1 <- as.vector(pop1FrqFile[,2])
+      varPop2 <- as.vector(pop2FrqFile[,2])
+      biallelicPop1 <- includeBiallelicOnly(varPop1)
+      biallelicPop2 <- includeBiallelicOnly(varPop2)
+      pop1FrqFileBiallelic <- pop1FrqFile[which(varPop1 %in% biallelicPop1),]
+      pop2FrqFileBiallelic <- pop2FrqFile[which(varPop2 %in% biallelicPop2),]
+      if (setequal(biallelicPop1, biallelicPop2) == FALSE) {
+          selectRows = findCommonVars(biallelicPop1, biallelicPop2)
+          pop1FrqDf = pop1FrqFileBiallelic[selectRows[[1]],]
+          pop2FrqDf = pop2FrqFileBiallelic[selectRows[[2]],]
       } else {
-          pop1FrqDf = pop1FrqFile
-          pop2FrqDf = pop2FrqFile
+          pop1FrqDf = pop1FrqFileBiallelic
+          pop2FrqDf = pop2FrqFileBiallelic
       }
-      frqCol1 <- as.vector(pop1FrqDf[,5])
-      frqCol1Split <- strsplit(frqCol1, split=":")
-      frq1 <- as.numeric(sapply(frqCol1Split, "[[", 2))
-      frqCol2 <- as.vector(pop2FrqDf[,5])
-      frqCol2Split <- strsplit(frqCol2, split=":")
-      frq2 <- as.numeric(sapply(frqCol2Split, "[[", 2))
+      frq1 <- extractFrequencies(as.vector(pop1FrqDf[,5]))
+      frq2 <- extractFrequencies(as.vector(pop2FrqDf[,5]))
       deltaVector <- c(deltaVector, frq2-frq1)
       
       ## Now we also create a file with the HDV information for that pairwise population
-      HDVpos <- which(abs(deltaVector) >= 0.6)
+      HDVpos <- which(abs(deltaVector) >= 0.4)
       if (length(HDVpos) > 0) {
         HDVcounter <- HDVcounter+length(HDVpos)
-        out <- rbind(out, cbind(pop1FrqFile[HDVpos,c(1,2,5)],pop2FrqFile[HDVpos,5]))
+        out <- rbind(out, cbind(pop1FrqDf[HDVpos,c(1,2,5)],pop2FrqDf[HDVpos,5]))
       }
       print(paste("end chr",chr))
     } #end chr
@@ -77,66 +74,33 @@ for (pop1 in 1:(length(popList)-1)) {
     if (HDVcounter > 0) { 
       write.table(out, paste("HDVList_",popList[pop1],"-",popList[pop2],"Delta60.txt",sep=""),quote=FALSE, col.names=FALSE, row.names=FALSE, sep="\t")
       ## Now we populate the matrix with pairwise pop HDVs    
-      HDVmat[pop1,pop2] <- sum(HDV1,HDV2,na.rm = T)
+      HDVmat[pop1,pop2] <- HDVcounter
     } else {
       ## Now we populate the matrix with pairwise pop HDVs    
       HDVmat[pop1,pop2] <- 0
     }
     ## Now we group the delta values in 0.05 frequency bins
-    frqBin <- c( (-80:80)/(40*2) )
-    binVector <- seq(-1,1,by=0.0500)
-    deltaVectorGrouped <- deltaVector[which(deltaVector != 0)]
-    numHitsBinTable <- table(deltaVectorGrouped)
-    numHitsBin = as.vector(numHitsBinTable) #DELTA_vector #as.vector(t(DeltaCounts[r,]))
-    frqList <- as.numeric(names(numHitsBinTable))
-    ## We need to round the numbers so that we don't have to deal with the nightmare of equals and decimals in R
-    frqList <- round(frqList, digits=2)
-    binVector <- round(binVector, digits=2)
-    v <- c()
-    for (bin in 1:length(binVector)) {
-      if (binVector[bin] < 0) {
-        v <- c(v, sum(numHitsBin[which(frqList >= binVector[bin] & frqList < binVector[bin+1] ) ]) )
-      }  
-      if (binVector[bin] > 0) {
-        v <- c(v, sum(numHitsBin[which(frqList > binVector[bin-1] & frqList <= binVector[bin] ) ]) )
-      }
-    }
+    binVector <- seq(-1,1,by=0.1)
+    deltaVectorNoZero <- deltaVector[which(deltaVector != 0)]
+    frqList <- round(deltaVectorNoZero, digits=1)
     # Now we save v so that we are able to know in the future what are the exact number of variants for each ferquency bin
     write.table(rbind(binVector[-which(binVector==0)],v), paste(popList[pop1],"-",popList[pop2], "_delta60Distribution.txt",sep=""), quote=FALSE, col.names=FALSE, row.names=FALSE, sep="\t")
-    # Now we plot. We generate a barplot and two zoomed in version focusing on HDVs
-    t <- barplot(v, plot=FALSE)
-    pos <- c(seq(1,20, by=2),seq(22,length(t),by=2))
-    lab = seq(-1,1,by=0.1)
-    lab = lab[-which(lab==0)]
     
-    pdf(file = paste("Figure1_",popList[pop1],popList[pop2],"_Delta60Distribution.pdf",sep=""),width = 16, height=7)
+    pdf(file = paste("Figure1_",popList[pop1],popList[pop2],"_Delta", delta, "Distribution.pdf",sep=""),width = 16, height=7)
     layout(matrix(c(0,1,0,2,1,3,2,1,3,2,1,3,0,1,0), nrow=5, ncol=3, byrow = TRUE), widths=c(2,8,2), height=1, respect = FALSE)
-    barplot(rev(v), col=c(rep("blue4",20),rep("gold",20)), main=paste(pop1,pop2,"Delta distribution"), cex.axis=1.4, cex.main=2 )
-    axis(1, at=t[pos], labels = lab, cex.axis=1.4)
-    
-    HDV1 <- v[which(binVector <= -0.6)]
-    HDV2 <- v[which(binVector >= 0.6)]
-    
-    if (sum(HDV1, HDV2, na.rm = T) > 0) {
-      if ( max(HDV1,HDV2,na.rm = T) < 10) {
+    hist(frqList, col="firebrick", breaks=binVector, xlab = paste("Delta =", delta, sep=" "), ylab="Number of variants", main=paste(popList[pop2],"vs.",popList[pop1], sep=" "), cex.axis=1.5 ) # Negative values are variants more frequent in Zambia.
+    HDV1 <- deltaVector[which(deltaVector <= -0.4)]
+    HDV2 <- deltaVector[which(deltaVector >= 0.4)]
+    if (HDVcounter > 0) {
+      if ( HDVcounter < 10) {
         yax_max <- 10
       } else {
-        yax_max <- yLimMax(max(HDV1,HDV2,na.rm = T),10^(nchar(max(HDV1,HDV2,na.rm = T))-1) )+ (10^(nchar(max(HDV1,HDV2,na.rm = T))-1)/2)
+        yax_max <- max(length(HDV1), length(HDV2))
       }
-      par(mar=c(5.1,1.1,0,1))
-      barplot(HDV1, col=rep("blue4",9), names.arg = seq(-1,-0.60,by=0.05), axes=FALSE, cex.names=1.5, ylim=c(0,yax_max) )
-      axis(4, at=seq(from=0, to=yax_max,length.out = 11), labels = , cex.axis=1.3)
-      yText = yax_max - (10^(nchar(max(HDV1,HDV2, na.rm = T))-1)/2)
-      if (sum(rev(v[which(bin >= 0.6)-1])) != 0 ) {
-        text(2.75,yText,paste(pop1,"=", formatC( sum(HDV1, na.rm = T), format="d",big.mark=","  ) ), cex=1.3)
-      }
-      par(mar=c(5.1,0,0,1.1))
-      barplot(HDV2, col=rep("gold",9), names.arg = seq(0.60,1,by=0.05), cex.names=1.5, axes=FALSE, ylim=c(0,yax_max) )
-      axis(2, at=seq(from=0, to=yax_max,length.out = 11), labels = , cex.axis=1.3)
-      #yText = max(rev(v[which(bin <= -0.6)]))/1.2
-      if (sum(rev(v[which(bin <= -0.6)])) != 0 ) {
-        text(8,yText,paste(pop2,"=",formatC( sum(HDV2,na.rm = T),format="d",big.mark = "," ) ), cex=1.3)
-      }
+      par(mar=c(5.1,2.1,2,1))
+      hist(round(HDV1, digits = 2), col = rep("blue4",9), breaks = seq(-1, -delta, by=0.05), ylim=c(0, yax_max), xlab = "", ylab="", main=paste(popList[pop2], "=", length(HDV1), sep = " ") )
+      par(mar=c(5.1,0,2,1.1))
+      hist(round(HDV2, digits=1), col=rep("gold",9), breaks = seq(delta,1,by=0.05), ylim=c(0, yax_max), xlab="", ylab="", main=paste(popList[pop1], "=", length(HDV2), sep="") )
     } 
     dev.off()
 
